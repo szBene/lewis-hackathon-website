f12b6364f0544a9e244c9e1ae615c3e6c4fecc7a
szBene <szbene.github@gmail.com>
2024-08-15 00:28:58 +0200

    navbar now closes when clicked outside of it

--- navbarToggle.js
+++ navbarToggle.js
@@ -1,5 +1,5 @@
 // toggles navbar items when hamburger icon is clicked
-document.getElementById("hamburger").addEventListener("click", function () {
+document.getElementById("hamburger").addEventListener("click", function (e) {
 	var navItems = document.getElementById("nav-items");
 	var navbar = document.getElementById("navbar");
 
@@ -8,4 +8,20 @@ document.getElementById("hamburger").addEventListener("click", function () {
 
 	// Toggle the expansion of the navbar
 	navbar.classList.toggle("expand");
+
+	// Stop the event from propagating to the document click listener
+	e.stopPropagation();
+});
+
+// Close the navbar if the user clicks outside of it
+document.addEventListener("click", function (e) {
+	var navItems = document.getElementById("nav-items");
+	var navbar = document.getElementById("navbar");
+
+	// Check if the navbar is expanded and if the click is outside the navbar
+	if (navbar.classList.contains("expand") && !navbar.contains(e.target)) {
+		// Close the navbar
+		navItems.classList.remove("show");
+		navbar.classList.remove("expand");
+	}
 });

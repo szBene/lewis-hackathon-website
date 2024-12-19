72c24a128d90c6fdbd5a0845d36ad5aba7b8919f
szBene <szbene.github@gmail.com>
2024-12-19 12:39:31 +0100

    fix navbar css

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -27,31 +27,40 @@
 	}
 </script>
 
-<nav bind:this={navbar} class="navbar {isExpanded ? 'expand' : ''}" id="navbar">
-	<div class="title-group">
-		<a class="nav-title" href="/">
-			<h1 class="title1 rubik fs-40 text-center font-semibold text-white">HACKATHON</h1>
-			<h2 class="title2 rubik fs-30 text-center font-light text-white">with Lewis</h2>
-		</a>
-		<button class="hamburger" on:click={toggleNavbar} id="hamburger" aria-label="hamburger">
-			<span></span>
-			<span></span>
-			<span></span>
-		</button>
-	</div>
-	<div class="nav-items {isExpanded ? 'show' : ''}" id="nav-items">
-		<a class="scp white-90 fs-20 text-center underline" href="/learnmore">Learn More</a>
-		<a class="scp white-90 fs-20 text-center underline" href="/rules">Rules</a>
-		<a class="scp white-90 fs-20 text-center underline" href="/theme">Theme</a>
-		<a
-			class="scp white-90 fs-20 text-center underline"
-			target="_blank"
-			href="https://forms.gle/sDpNYnAcsryDDShx8">Sign Up</a
-		>
-		<a
-			class="scp white-90 fs-20 text-center font-semibold underline"
-			target="_blank"
-			href="https://forms.gle/9bA1BEmaV7VvrZ2z8">Submit</a
-		>
-	</div>
-</nav>
+<!--
+do not change navbar structure
+the border gradient effect depends on this specific structure
+(see in app.css)
+
+you can add elements if you want
+-->
+<div class="navbar {isExpanded ? 'expand' : ''}" id="navbar">
+	<nav bind:this={navbar}>
+		<div class="title-group">
+			<a class="nav-title" href="/">
+				<h1 class="title1 rubik fs-40 text-center font-semibold text-white">HACKATHON</h1>
+				<h2 class="title2 rubik fs-30 text-center font-light text-white">with Lewis</h2>
+			</a>
+			<button class="hamburger" on:click={toggleNavbar} id="hamburger" aria-label="hamburger">
+				<span></span>
+				<span></span>
+				<span></span>
+			</button>
+		</div>
+		<div class="nav-items {isExpanded ? 'show' : ''}" id="nav-items">
+			<a class="scp white-90 fs-20 text-center underline" href="/learnmore">Learn More</a>
+			<a class="scp white-90 fs-20 text-center underline" href="/rules">Rules</a>
+			<a class="scp white-90 fs-20 text-center underline" href="/theme">Theme</a>
+			<a
+				class="scp white-90 fs-20 text-center underline"
+				target="_blank"
+				href="https://forms.gle/sDpNYnAcsryDDShx8">Sign Up</a
+			>
+			<a
+				class="scp white-90 fs-20 text-center font-semibold underline"
+				target="_blank"
+				href="https://forms.gle/9bA1BEmaV7VvrZ2z8">Submit</a
+			>
+		</div>
+	</nav>
+</div>

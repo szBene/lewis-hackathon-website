f4b4468145447bcb08551b7452be060952a8d7cd
szBene <szbene.github@gmail.com>
2024-12-20 23:54:09 +0100

    fix mobile layout issues with media (img, video, iframe)

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -16,7 +16,7 @@
 			<div class="winner-item text-left">
 				<p class="scp white-90 fs-20 mb-1 mt-3 font-semibold">Milk Lang</p>
 				<a href="https://github.com/6043/milk_lang/" target="_blank">
-					<img src="assets/results/project1.png" alt="Project 1" class="mb-2 block h-auto w-full" /></a
+					<img src="assets/results/project1.png" alt="Project 1" /></a
 				>
 				<p class="scp white-90 fs-20"><span class="font-semibold">Members:</span> Addy</p>
 				<p class="scp white-90 fs-20">
@@ -29,7 +29,7 @@
 			<div class="winner-item text-left">
 				<p class="scp white-90 fs-20 mb-1 mt-3 font-semibold">There is No Website</p>
 				<a href="https://github.com/AndreiBalan-dev/there-is-no-website" target="_blank"
-					><img src="assets/results/project2.png" alt="Project 1" class="mb-2 block h-auto w-full" /></a
+					><img src="assets/results/project2.png" alt="Project 1" /></a
 				>
 				<p class="scp white-90 fs-20">
 					<span class="font-semibold">Members: </span>Balan Andrei, Rudransh Joshi and Andy Mai
@@ -44,7 +44,11 @@
 			<div class="winner-item text-left">
 				<p class="scp white-90 fs-20 mb-1 mt-3 font-semibold">Poser Detector</p>
 				<a href="https://github.com/deniz-dilaverler/poser-detector" target="_blank"
-					><img src="assets/results/project3.png" alt="Project 1" class="mb-2 block h-auto w-full" /></a
+					><img
+						src="assets/results/project3.png"
+						alt="Project 1"
+						class="mb-2 block h-auto w-full"
+					/></a
 				>
 				<p class="scp white-90 fs-20">
 					<span class="font-semibold">Members:</span> Deniz Met Dilaverer and Ceren Akyar
@@ -56,15 +60,15 @@
 				</p>
 			</div>
 		</div>
-		<p class="section-body scp white-90 fs-16 mt-8">
+		<p class="section-body scp white-90 fs-20 mt-8">
 			Congratulations to the winners! But there were so many amazing projects submitted, sadly we
 			can't showcase them all here. Go check out the YouTube Video where you can see a few more
 			projects that were submitted.
 		</p>
 
 		<iframe
-			width="560"
-			height="315"
+			width="960"
+			height="540"
 			src="https://www.youtube.com/embed/1hgN3mwE7jc?si=0Crw0bTNUhK-eYET"
 			title="YouTube video player"
 			frameborder="0"
--- src/routes/theme/+page.svelte
+++ src/routes/theme/+page.svelte
@@ -7,7 +7,7 @@
 		<h3 class="section-title rubik white-90 fs-40 text-center font-semibold">Theme</h3>
 		<h2 class="section-body scp white-90 fs-40 text-center">Functionally Dysfunctional</h2>
 		<div class="flex items-center justify-center">
-			<img src="assets/theme/themethumbnail.png" alt="" width="50%" class="text-center" />
+			<img src="assets/theme/themethumbnail.png" alt="" width="50%" />
 		</div>
 		<p class="section-body scp white-90 fs-20">
 			We are looking for people to be creative by building a project that is functional but has some
@@ -23,15 +23,19 @@
 				<video src="assets/theme/example1.mp4" controls class="block h-auto w-full">
 					<track kind="captions" />
 				</video>
-				<p class="block">A video player that only plays video when you balance the bar.</p>
+				<p class="section-body scp fs-16">
+					A video player that only plays video when you balance the bar.
+				</p>
 			</div>
 			<div class="example-item text-left">
-				<img src="assets/theme/example2.png" alt="Example 2" class="h-auto w-full" />
-				<p>A captcha that forces you to play minesweeper to pass.</p>
+				<img src="assets/theme/example2.png" alt="Example 2" />
+				<p class="section-body scp fs-16">A captcha that forces you to play minesweeper to pass.</p>
 			</div>
 			<div class="example-item text-left">
-				<img src="assets/theme/example3.png" alt="Example 3" class="h-auto w-full" />
-				<p>A terminal application that forces the player to think in strange ways.</p>
+				<img src="assets/theme/example3.png" alt="Example 3" />
+				<p class="section-body scp fs-16">
+					A terminal application that forces the player to think in strange ways.
+				</p>
 			</div>
 		</div>
 		<p class="section-body scp white-90 fs-20">
